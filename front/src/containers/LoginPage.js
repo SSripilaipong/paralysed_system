c6e67706d3a5a4125fbace54dd57bad27c479bd5
a526735a <31316439+a526735a@users.noreply.github.com>
2019-03-21 17:16:24 +0700

    again

--- front/src/containers/LoginPage.js
+++ front/src/containers/LoginPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./LoginPage.css"
+import "./LoginPage.css";
 import axios from "axios";
 import {
   FormGroup,
@@ -24,7 +24,10 @@ class LoginPage extends Component {
       event.stopPropagation();
     }
     this.setState({ validated: true });
-    const data = { ...this.state };
+    const data = {
+      username: this.state.username,
+      password: this.state.password
+    };
     if (form.checkValidity()) {
       axios
         .post("http://localhost:8000/api/login", data)
@@ -47,9 +50,7 @@ class LoginPage extends Component {
   render() {
     const { validated } = this.state;
     return (
-      
       <div className="Login">
-        
         <Form onSubmit={this.loginHandler} noValidate validated={validated}>
           <p className="center_text">MEMBER LOGIN</p>
           <FormGroup controlId="username" size="large">
@@ -89,7 +90,6 @@ class LoginPage extends Component {
           <a href="regis">Register</a>
         </Form>
       </div>
-
     );
   }
 }
